0db7a01208be36c838b3e4bf926d89b73cf3ce4c
raul <raul@zamacona.me>
2014-07-20 19:48:53 -0500

    muchas secciones

--- js/functions.js
+++ js/functions.js
@@ -4,8 +4,16 @@
 
 	$(function(){
 
-		$('.beneficio').on('click', function(){
+		$('.beneficio').on('mouseenter', function(){
 			disable( $(this), '.beneficio' );
+			var slide = $(this).data('slide');
+			//console.log('s'+slide);
+			$('.slideshow-beneficios').cycle('goto', slide);
+		});
+
+		headerScroll();
+		$(document).on( 'scroll', function(){
+			headerScroll();
 		});
 
 	});
@@ -15,4 +23,15 @@
 function disable(elemento, hermanos){
 	$(hermanos).removeClass('abled').addClass('disabled');
 	elemento.removeClass('disabled').addClass('abled');
+}
+
+function headerScroll(){
+	var alturaHeader = $('header').height();
+	var scrollTop = $(window).scrollTop();
+
+	if ( scrollTop >= alturaHeader ){
+		$('header').addClass('scroll');
+	} else {
+		$('header').removeClass('scroll');
+	}
 }
