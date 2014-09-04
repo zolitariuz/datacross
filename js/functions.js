68976b7bcbdcc06abbf2b52d565c2671c3e48d94
raul <raul@zamacona.me>
2014-09-03 20:22:16 -0500

    forma-contacto validate

--- js/functions.js
+++ js/functions.js
@@ -30,8 +30,85 @@
 		$(document).on( 'scroll', function(){
 			headerScroll();
 		});
-		
-    $(".video-ejemplos").fitVids();
+
+		$(".video-ejemplos").fitVids();
+
+		$('.forma-contacto').validate();
+
+		//nuevo_slider
+		if( $('.tp-banner').length > 0 ){
+			$('.tp-banner').show().revolution({
+				dottedOverlay:"none",
+				delay:7000,
+				startwidth:1170,
+				startheight:500,
+				hideThumbs:200,
+
+				thumbWidth:100,
+				thumbHeight:50,
+				thumbAmount:5,
+
+				navigationType:"none",
+				navigationArrows:"none",
+				navigationStyle:"none",
+
+				touchenabled:"on",
+				onHoverStop:"on",
+
+				swipe_velocity: 0.7,
+				swipe_min_touches: 1,
+				swipe_max_touches: 1,
+				drag_block_vertical: false,
+
+				parallax:"mouse",
+				parallaxBgFreeze:"on",
+				parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
+
+				keyboardNavigation:"off",
+
+				navigationHAlign:"center",
+				navigationVAlign:"bottom",
+				navigationHOffset:0,
+				navigationVOffset:20,
+
+				soloArrowLeftHalign:"left",
+				soloArrowLeftValign:"center",
+				soloArrowLeftHOffset:20,
+				soloArrowLeftVOffset:0,
+
+				soloArrowRightHalign:"right",
+				soloArrowRightValign:"center",
+				soloArrowRightHOffset:20,
+				soloArrowRightVOffset:0,
+
+				shadow:0,
+				fullWidth:"on",
+				fullScreen:"off",
+
+				spinner:"spinner4",
+
+				stopLoop:"off",
+				stopAfterLoops:-1,
+				stopAtSlide:-1,
+
+				shuffle:"off",
+
+				autoHeight:"off",
+				forceFullWidth:"off",
+
+				hideThumbsOnMobile:"off",
+				hideNavDelayOnMobile:1500,
+				hideBulletsOnMobile:"off",
+				hideArrowsOnMobile:"off",
+				hideThumbsUnderResolution:0,
+
+				hideSliderAtLimit:0,
+				hideCaptionAtLimit:0,
+				hideAllCaptionAtLilmit:0,
+				startWithSlide:0,
+				fullScreenOffsetContainer: ".header"
+			});
+		}
 
 	});
 
@@ -49,87 +126,6 @@ $(document).ready(function() {
         $(tab).fadeIn();
     });
 });
-
-//nuevo_slider
-
-jQuery(document).ready(function() {
-	
-					jQuery('.tp-banner').show().revolution(
-					{
-						dottedOverlay:"none",
-						delay:7000,
-						startwidth:1170,
-						startheight:500,
-						hideThumbs:200,
-						
-						thumbWidth:100,
-						thumbHeight:50,
-						thumbAmount:5,
-						
-						navigationType:"none",
-						navigationArrows:"none",
-						navigationStyle:"none",
-						
-						touchenabled:"on",
-						onHoverStop:"on",
-						
-						swipe_velocity: 0.7,
-						swipe_min_touches: 1,
-						swipe_max_touches: 1,
-						drag_block_vertical: false,
-												
-						parallax:"mouse",
-						parallaxBgFreeze:"on",
-						parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
-												
-						keyboardNavigation:"off",
-						
-						navigationHAlign:"center",
-						navigationVAlign:"bottom",
-						navigationHOffset:0,
-						navigationVOffset:20,
-
-						soloArrowLeftHalign:"left",
-						soloArrowLeftValign:"center",
-						soloArrowLeftHOffset:20,
-						soloArrowLeftVOffset:0,
-
-						soloArrowRightHalign:"right",
-						soloArrowRightValign:"center",
-						soloArrowRightHOffset:20,
-						soloArrowRightVOffset:0,
-								
-						shadow:0,
-						fullWidth:"on",
-						fullScreen:"off",
-
-						spinner:"spinner4",
-						
-						stopLoop:"off",
-						stopAfterLoops:-1,
-						stopAtSlide:-1,
-
-						shuffle:"off",
-						
-						autoHeight:"off",						
-						forceFullWidth:"off",						
-					
-												
-						hideThumbsOnMobile:"off",
-						hideNavDelayOnMobile:1500,						
-						hideBulletsOnMobile:"off",
-						hideArrowsOnMobile:"off",
-						hideThumbsUnderResolution:0,
-						
-						hideSliderAtLimit:0,
-						hideCaptionAtLimit:0,
-						hideAllCaptionAtLilmit:0,
-						startWithSlide:0,
-						fullScreenOffsetContainer: ".header"	
-					});
-
-				});	
-//ready
 
 
 function disable(elemento, hermanos){
